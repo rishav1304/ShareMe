58c64f23051d03809639f63c4cd4b78ea3077feb
rishav1304 <rishav.kumar21b@iiitg.ac.in>
2024-06-22 23:39:25 +0530

    change-1

--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -4,7 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
-
+// import './index.css';
+import './index.css';
 ReactDOM.render(
   <Router>
     <App />
